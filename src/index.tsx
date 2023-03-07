b5ecc25fa058b3a747e676b110520c87cb285701
Jeff Olson <jolson615@gmail.com>
2023-03-07 13:42:31 -0500

    Router => RouterProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
